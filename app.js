d68e90f06a28b5da3ed3cc7f91efb79e0a8b5125
Mohamed Suwan <67264950+MohamedSuwan@users.noreply.github.com>
2022-06-19 15:39:29 +0300

    Update app.js

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ particlesJS('particles-js',
         "type": "circle",
         "stroke": {
           "width": 0,
-          "color": "#000000"
+          "color": "#58A6FF"
         },
         "polygon": {
           "nb_sides": 5
@@ -119,7 +119,7 @@ particlesJS('particles-js',
         }
       }
     },
-    "retina_detect": true,
+    "retina_detect": false,
     "config_demo": {
       "hide_card": false,
       "background_color": "#b61924",
